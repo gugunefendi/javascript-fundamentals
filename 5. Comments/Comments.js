24f69dea95d33204994f4c7f3a74bd9e22a7c680
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-08-27 21:44:42 +0700

    Lesson 5: Comments

---
+++ 5. Comments/Comments.js
@@ -0,0 +1,13 @@
+// Single-line comment
+var x = 5; // This is a single line comment
+
+// Multi-line comment
+/* This is a multi-line comment 
+These is a javascript fundamentals repository.
+It contains all the necessary information to learn JavaScript.
+*/
+
+// Block comment
+/* if (x > 3) {
+  console.log("x is greater than 3");
+} */
