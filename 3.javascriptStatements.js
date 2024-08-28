1360c82dcedc3af135a1926b2ae3e64102df292e
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-08-28 23:09:33 +0700

    Simplified Lesson 3: Statements

---
+++ 3.javascriptStatements.js
@@ -0,0 +1,31 @@
+/*
+NOTE:
+Pisahkan setiap statement dengan tanda titik koma (;)
+Tambahkan spasi untuk memperindah dan memperjelas kode, terutama di sekitar operator ( + - % = / * )
+Panjang baris terbaik tidak lebih dari 80 karakter, jika statement tidak muat dalam 1 baris putuskan kode setelah operator
+*/
+
+/*
+Contoh statement:
+statement ini memberitahu browser untuk menampilkan teks "Hello JavaScript!" pada elemen dengan id "demo"
+*/
+document.getElementById("demo").textContent = "Hello JavaScript!";
+
+// pisahkan setiap statement dengan tanda titik koma (;)
+var x = 5;
+var y = 10;
+var z = x + y;
+
+// tambahkan spasi untuk memperindah dan memperjelas kode, terutama di sekitar operator ( + - % = / * )
+var a = 10;
+var b = 5;
+var c = ( a + b ) * 2;
+
+// panjang baris terbaik tidak lebih dari 80 karakter, jika statement tidak muat dalam 1 baris putuskan kode setelah operator
+const userOne = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+// kode diatas lebih indah dan lebih mudah dibaca daripada
+const userTwo = { firstName: "John", lastName: "Doe", age: 30 };
