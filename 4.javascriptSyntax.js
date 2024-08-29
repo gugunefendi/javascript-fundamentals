2781797d9ea37d41193f34ba7192645cf492547d
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-08-29 10:17:24 +0700

    Simplified Lesson 4: Javascript Syntax

---
+++ 4.javascriptSyntax.js
@@ -0,0 +1,46 @@
+/*
+Syntax membuat variabel:
+    1. var
+    2. let
+    3. const
+Gunakan = untuk memberikan value kedalam variabel:
+Value memiliki 2 jenis:
+    1. value tetap (literals)
+    2. value variabel (variables)
+Operator:
+    1. operator aritmatika (+, -, *, /, %)
+    2. operator penugasan (=)
+Komentar:
+    1. //untuk single line
+    2. untuk multi line menggunakan slash bintang
+Camel Case:
+Programmer Javascript cenderung menggunakan Camel Case untuk menulis nama variabel.
+*/
+
+// membuat variabel
+var x;
+let y;
+const z = "Hello Javascript";
+
+// memberikan value kedalam variabel
+x = 10;
+y = 20;
+
+// value
+//value tetap
+let firstScore = 10; 
+let secondScore = 20;
+// value variabel
+let totalScore = firstScore + secondScore;
+
+// operator
+let result = 10 + 20;
+let division = 10 / 2;
+// operator = digunakan untuk memberikan value kepada variabel
+
+// komentar
+// untuk single line
+/*  
+ini adalah                    
+komentar multi line
+*/
