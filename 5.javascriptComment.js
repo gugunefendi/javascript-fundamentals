9e4e03f51f1883dd216b7568deabf56888eb6c65
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-08-29 10:29:27 +0700

    Simplified Lesson 5: Comments

---
+++ 5.javascriptComment.js
@@ -0,0 +1,24 @@
+/*
+Javascript comments:
+1. single line comment: //
+2. multi-line comments: slash bintang bintang slash
+3. documentation comments: slash bintang bintang bintang slash
+*/
+
+// single line comment
+let name = "John"; // This is a single line comment
+
+/*
+multi-line comment
+if(name === "John") {
+    console.log("Hello John");
+}
+*/
+
+/**
+ * documentation comments
+ * @param {string} name - The name of the person
+ * @returns {string} - A greeting message
+ * @example
+ * greet("John"); // Output: "Hello John"
+ */
