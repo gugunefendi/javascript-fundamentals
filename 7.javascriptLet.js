8f57e5b5454bad0d753a0da17d5f76c0cb24ba69
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-09-02 10:51:39 +0700

    Lesson 7: Different of let and var

--- 7.javascriptLet.js
+++ 7.javascriptLet.js
@@ -1,20 +1,56 @@
 /*
-Kata kunci 'let' di perkenalkan di ES6 (2015).
-Variabel yang di deklarasikan menggunakan 'let' harus di deklarasikan sebelum digunakan.
-Variabel yang di deklarasikan menggunakan 'let' tidak bisa di deklarasikan ulang didalam scope yang sama.
+1. Let adalah variabel blok scope;
+2. Let tidak dapat di deklarasikan ulang di scope yang sama;
+3. Let tidak dapat di hoisting;
+
+4. Var adalah variabel global scope;
+5. Var dapat di deklarasikan ulang di scope yang sama;
+6. Var dapat di hoisting; artinya: kita dapat menggunakan variabel tersebut sebelum variabel tersebut di deklarasikan.
 */
 
-/*
-Sebelum ES6 (2015) javascript tidak memiliki Block Scope.
-Javascript hanya memiliki Global Scope dan Function Scope.
-ES6 memperkenalkan 2 keyword baru yaitu let dan const.
-Dua keyword ini menyediakan Block Scope.
-*/
-
-/*
-Contoh:
+// 1. Let adalah variabel block scope
 {
-    let x = 10;
+    let score = 100;
 }
-console.log(x); // ini akan error, karena variabel x di deklarasikan di dalam block scope.
-*/
+// console.log(score); // error
+
+// 2. Let tidak dapat di deklarasikan ulang di scope yang sama
+let personOne = 'John';
+// let personOne = 'Jane'; // error
+console.log(personOne);
+
+{
+    let personTwo = 'Adi';
+    // let personTwo = 'Budi'; // error
+}
+let personTwo = 'Cici';
+console.log(personTwo);
+
+// 3. Let tidak dapat di hoisting;
+personSix = 'Icha';
+// let personSix = 'Amel';
+console.log(personSix); // error
+
+
+
+// 4. Var adalah variabel global scope
+{
+    var globalName = 'global';
+}
+console.log(globalName); // dapat diakses di luar scope
+
+// 5. Var dapat di deklarasikan ulang di scope yang sama;
+var personThree = 'Dani';
+var personThree = 'Eva';
+console.log(personThree); // output Eva
+
+var personFour = 'Dina';
+{
+    var personFour = 'Cici';
+}
+console.log(personFour); // output Cici. Dapat di re-assign meski berbeda scope
+
+// Var dapat di hoisting;
+personFive = 'Hadi';
+var personFive;
+console .log(personFive); // output: Hadi
